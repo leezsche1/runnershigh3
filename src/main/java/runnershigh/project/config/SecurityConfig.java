a7acd9f8c4c8243b962f32fc31d2a65dbdd24a6a
Seyeon Lee <sseyeonida@gmail.com>
2024-04-18 15:30:06 +0900

    first commit

--- src/main/java/runnershigh/project/config/SecurityConfig.java
+++ src/main/java/runnershigh/project/config/SecurityConfig.java
@@ -1,16 +1,36 @@
 package runnershigh.project.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import runnershigh.project.security.exception.CustomAuthenticationEntryPoint;
+import runnershigh.project.security.filter.JwtAuthenticationFilter;
+import runnershigh.project.security.provider.JwtAuthenticationProvider;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -29,6 +49,22 @@ public class SecurityConfig {
 
         http
                 .formLogin((auth) -> auth.disable());
+
+        http
+                .logout((auth) -> auth
+                        .disable());                //로그아웃은 disable로 해줘야 한다. 안그러면 member controller에 있는 /logout url이 통하지 않는다.
+
+        http
+                .exceptionHandling((auth) -> auth
+                        .authenticationEntryPoint(customAuthenticationEntryPoint));
+
+
+        http
+                .authenticationProvider(jwtAuthenticationProvider);
+
+        http
+                .addFilterBefore(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
 
         http
                 .csrf((auth) -> auth.disable());
